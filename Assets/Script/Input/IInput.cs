fe39c44d80e88b56710ba877c6873d849aefb1a7
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2024-04-14 23:55:01 -0500

    Assets

---
+++ Assets/Script/Input/IInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public interface IMovable
+{
+    void FixedUpdate(); 
+    void Update();
+    void OnCollisionEnter(Collision collision);
+    void OnCollisionExit(Collision collision);
+    void OnTriggerEnter(Collider other);
+    void OnTriggerExit(Collider other);
+}
